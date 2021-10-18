c5d90cd327c892ba5973fa76d0f0f9539b09e76d
Zélie <alchami.zelie@gmail.com>
2021-10-18 11:05:00 +0200

    Personnalisation du menu admin

---
+++ plugins/zelie/menus/Plugin.php
@@ -0,0 +1,51 @@
+<?php namespace Zelie\Menus;
+
+use Event;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function boot()
+    {
+        Event::listen('backend.menu.extendItems', function($manager) {
+
+            $manager->addMainMenuItems('Raviraj.Rjgallery', [
+                'gallery' => [
+                    'label' => 'Phototèque',
+                    'icon' => 'icon-camera-retro'
+                ]
+            ]);
+
+            $manager->addMainMenuItems('Rainlab.User', [
+                'user' => [
+                    'label' => 'Abonnés',
+                    'icon' => 'icon-users',
+                    'iconSvg' => ''
+
+                ]
+            ]);
+
+            $manager->addMainMenuItems('Martin.Forms', [
+                'forms' => [
+                    'label' => 'Messages',
+                    'icon' => 'icon-envelope-o',
+                    'iconSvg' => ''
+                ]
+            ]);
+
+            $manager->addMainMenuItems('Lovata.Ordersshopaholic', [
+                'orders-shopaholic-menu' => [
+                    'label' => 'Commandes'
+                ]
+            ]);
+
+            $manager->addMainMenuItems('Lovata.Shopaholic', [
+                'shopaholic-menu-main' => [
+                    'icon' => 'icon-book'
+                ]
+            ]);
+
+        
+        });
+    }
+}
